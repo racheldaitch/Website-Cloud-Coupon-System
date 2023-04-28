fc735c51a9106450bdb9735d153c452e2862f446
racheldaitch <racheldaitch@gmail.com>
2023-04-28 11:18:30 +0300

    Update Config.ts

--- src/Utils/Config.ts
+++ src/Utils/Config.ts
@@ -11,13 +11,13 @@ class DevelopmentConfig {
 }
 
 class ProductionConfig {
-    public adminUrl = "http://localhost:8080/api/admin/";
-    public companyUrl = "http://localhost:8080/api/company/";
-    public customerUrl = "http://localhost:8080/api/customer/";
-    public authUrl = "http://localhost:8080/auth/login";
-    public allCoupons = "http://localhost:8080/allCoupons";
+    public adminUrl = "https://server-cloud-coupon-system-production.up.railway.app/api/admin/";
+    public companyUrl = "https://server-cloud-coupon-system-production.up.railway.app/api/company/";
+    public customerUrl = "https://server-cloud-coupon-system-production.up.railway.app/api/customer/";
+    public authUrl = "https://server-cloud-coupon-system-production.up.railway.app/auth/login";
+    public allCoupons = "https://server-cloud-coupon-system-production.up.railway.app/allCoupons";
 }
 
-const appConfig = process.env.NODE_ENV === "development"
-    ? new DevelopmentConfig() : new ProductionConfig();
+const appConfig = process.env.NODE_ENV == "production"
+    ?  new ProductionConfig() : new DevelopmentConfig();
 export default appConfig;
