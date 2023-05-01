3101ad5423b099ec959afe7c6d57de49118de924
racheldaitch <racheldaitch@gmail.com>
2023-05-02 02:17:35 +0300

    Update GetCustomerDetails.tsx

--- src/Components/CustomerArea/GetCustomerDetails/GetCustomerDetails.tsx
+++ src/Components/CustomerArea/GetCustomerDetails/GetCustomerDetails.tsx
@@ -24,13 +24,14 @@ function GetCustomerDetails(): JSX.Element {
 
     return (
         <div className="GetCustomerDetails">
-            {customer ?
+            {customer == undefined && <Loading/>}
+            {customer != undefined &&
                 <div>
                     <h3>Name: {customer.firstName} {customer.lastName}</h3>
                     <hr />
                     <h4>Email: {customer.email}</h4>
                     <h4>Password: {customer.password}</h4>
-                </div> : <Loading/>
+                </div> 
             }
         </div>
     );
